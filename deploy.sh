49b1c8851bf6b6e069633a0eb9823d777e66f475
韩大伟 <779894759@qq.com>
2018-08-08 15:10:52 +0800

    add war file auth

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ cd $PROJ_PATH/test
 # 删除原有工程
 rm -rf /target
 
-# 编译生成新的项目工程
-mvn clean install
+# 忽略测试编译生成新的项目工程
+mvn clean install -Dmaven.test.skip=true
 
 # 启动新项目
-java -jar /target/test-0.0.1-SNAPSHOT.jar
+java -jar target/test-0.0.1-SNAPSHOT.jar
